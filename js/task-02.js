60627b937b56d637f447ee23671604c8e16cf0c9
eddvornichenko <127417581+eddvornichenko@users.noreply.github.com>
2023-07-13 00:02:12 +0300

    Update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,19 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const ingredientsList = document.getElementById("ingredients");
+
+ingredients.forEach((ingredient) => {
+  const listItem = document.createElement("li");
+
+  listItem.classList = "item";
+  listItem.textContent = ingredient;
+
+  ingredientsList.append(listItem);
+});
