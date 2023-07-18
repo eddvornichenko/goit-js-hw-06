0d4d711c47cc69a988c132a7e8d49af168691ffc
eddvornichenko <127417581+eddvornichenko@users.noreply.github.com>
2023-07-18 15:23:16 +0300

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,16 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const btnChangeColor = document.querySelector('button[type="button"]');
+const body = document.body;
+const spanColor = document.querySelector(".color");
+
+btnChangeColor.addEventListener("click", changeBg);
+
+function changeBg(event) {
+  const currentClr = getRandomHexColor();
+  console.log(currentClr);
+  body.style.backgroundColor = `${currentClr}`;
+  spanColor.textContent = `${currentClr}`;
+}
