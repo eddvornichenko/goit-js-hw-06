e5d3ca0e7175cda8dfc3189889e3327e40a80a81
eddvornichenko <127417581+eddvornichenko@users.noreply.github.com>
2023-07-18 14:05:14 +0300

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,22 @@
+const loginForm = document.querySelector(".login-form");
+const inputEmail = document.querySelector('input[type="email"');
+const inputPass = document.querySelector('input[type="password"');
+const btnSubmit = document.querySelector('button[type="submit"');
 
+loginForm.addEventListener("submit", onLoginSubmit);
+
+function onLoginSubmit(event) {
+  event.preventDefault();
+
+  const {
+    elements: { email, password },
+  } = event.currentTarget;
+
+  if (email.value === "" || password.value === "") {
+    alert("Все поля должны быть заполнены");
+  } else {
+    const userInfo = { email: email.value, password: password.value };
+    console.log(userInfo);
+    loginForm.reset();
+  }
+}
